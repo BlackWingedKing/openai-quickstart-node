deec5f32a2c741c8fb905885f4848db25ea23d8d
BlackWingedKing <ritesh.goru@gmail.com>
2023-02-02 23:27:08 +0530

    replace animal related stuff :P

--- pages/index.js
+++ pages/index.js
@@ -40,16 +40,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Write something and hit enter</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="query with prompt goes here"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
