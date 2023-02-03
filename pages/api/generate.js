b03e9e61381a6308abae1f8123b5ade49a16b4ea
BlackWingedKing <ritesh.goru@gmail.com>
2023-02-03 11:30:19 +0530

    nump max_tokens to 100

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,6 +30,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: animal,
       temperature: 0.6,
+      max_tokens: 100,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
